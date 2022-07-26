f3054e3870fb096c7e559f5b77634cafd657ab5a
soorya1235 <sarvanmls@gmail.com>
2022-07-26 11:08:03 +0530

    new commit

--- TestData/HomePageData.py
+++ TestData/HomePageData.py
@@ -1,3 +1,22 @@
+import openpyxl
+
+
 class HomePageData:
 
-    home_page_data = [{"firstname":"soorya","email":"abc.com","pass":"test","gender":"Male"},{"firstname":"ramesh","email":"ramesh.com","pass":"test","gender":"Female"}]
+    home_page_data = [{"firstname":"Rahul","lastname":"shetty","gender":"Male"}, {"firstname":"Anshika", "lastname":"shetty", "gender":"Female"}]
+
+    @staticmethod
+    def getTestData(test_case_name):
+        Dict = {}
+        book = openpyxl.load_workbook("C:\\Final_Framework\\Sel_Framework\\TestData\\excel_data.xlsx")
+        sheet = book.active
+        for i in range(1, sheet.max_row + 1):  # to get rows
+            if sheet.cell(row=i, column=1).value == test_case_name:
+
+                for j in range(2, sheet.max_column + 1):  # to get columns
+                    # Dict["lastname"]="shetty
+                    Dict[sheet.cell(row=1, column=j).value] = sheet.cell(row=i, column=j).value
+        return[Dict]
+
+
+
